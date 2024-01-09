fc8616c9bfc5a14a661d15037452e61e9e26407b
ToxPuro <purotouko2@gmail.com>
2024-01-09 13:21:57 +0200

    linking taskhandle from scheduler now

--- tests/fortran-interop/c.cpp
+++ tests/fortran-interop/c.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
-typedef struct
-{
-  const int task_id;
-} TaskHandle;
+#include <scheduler.h>
+extern "C"{
 
 TaskHandle
 call_it (TaskHandle (*func)(TaskHandle), TaskHandle arg)
 {
   printf("Hi from c: %d\n", arg.task_id);
   return func (arg);
+}
 }
